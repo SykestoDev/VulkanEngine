a512c363e6f8508d2ac84ec3cfd02b3dac0786ff
John Hernandez <Johncarlos4284@gmail.com>
2023-08-29 16:37:34 -0400

    Setup Build System & linked against Vulkan

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+
+SET includes=/Isrc /I%VULKAN_SDK%/Include
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib
+SET defines=/D DEBUG
+
+
+echo "Building main..."
+
+cl /EHsc %includes% %defines% src/main.cpp %links%
